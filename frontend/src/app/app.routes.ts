507131db70f770de69bd6c1cb02f0e05d9baeb7a
Daniel Villacis <Daniel1997v@gmail.com>
2025-02-06 17:00:43 -0500

    frontend beautified

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './home-page/home-page.component';
+import { ScanPageComponent } from './scan-page/scan-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'scan', component: ScanPageComponent }
+];
