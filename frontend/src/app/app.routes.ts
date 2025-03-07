76323f25845aa40c100f9e4e7b8efa17d268477f
Daniel Villacis <Daniel1997v@gmail.com>
2025-03-06 19:19:25 -0500

    cleaned the routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { ScanPageComponent } from './scan-page/scan-page.component';
+import { ClientPageComponent } from './client-page/client-page.component';
+import { CreateClientPageComponent } from './create-client-page/create-client-page.component';
 
 export const routes: Routes = [
-    { path: '', component: AppComponent },
-    { path: 'scan', component: ScanPageComponent }
+    { path: '', component: HomePageComponent },
+    { path: 'scan', component: ScanPageComponent },
+    { path: 'clients', component: ClientPageComponent },
+    // { path: 'clients/:id', component: ClientDetailComponent },
+    { path: 'clients/new', component: CreateClientPageComponent }
 ];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,18 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app-component/app.component';
-import { HomePageComponent } from './app/home-page/home-page.component';
-import { ScanPageComponent } from './app/scan-page/scan-page.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'scan', component: ScanPageComponent }
-];
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter(routes), provideAnimationsAsync()
+    provideRouter(routes), 
+    provideAnimationsAsync(), provideAnimationsAsync()
   ],
 }).catch(err => console.error(err));
