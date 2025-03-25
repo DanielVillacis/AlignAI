98cf0dbe58fdc7a451a609910e6c10d543ec09d3
Daniel Villacis <Daniel1997v@gmail.com>
2025-03-25 00:01:28 -0400

    event creation encapsulation

--- frontend/src/app/components/create-event-form/create-event-form.component.ts
+++ frontend/src/app/components/create-event-form/create-event-form.component.ts
@@ -22,58 +22,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatSelectModule,
     MatCheckboxModule
   ],
-  template: `
-    <h2 mat-dialog-title>{{data.title}}</h2>
-    <mat-dialog-content>
-      <form [formGroup]="eventForm">
-        <div class="form-group">
-          <mat-form-field appearance="outline" style="width: 100%;">
-            <mat-label>Title</mat-label>
-            <input matInput formControlName="title" placeholder="Event title">
-          </mat-form-field>
-        </div>
-        
-        <div class="form-group">
-          <mat-form-field appearance="outline" style="width: 100%;">
-            <mat-label>Description</mat-label>
-            <textarea matInput formControlName="description" placeholder="Event description" rows="3"></textarea>
-          </mat-form-field>
-        </div>
-        
-        <div class="form-group">
-          <mat-form-field appearance="outline" style="width: 100%;">
-            <mat-label>Time</mat-label>
-            <input matInput type="time" formControlName="event_time">
-          </mat-form-field>
-        </div>
-        
-        <div class="form-group">
-          <mat-form-field appearance="outline" style="width: 100%;">
-            <mat-label>Patient (Optional)</mat-label>
-            <mat-select formControlName="client_id">
-              <mat-option [value]="null">None</mat-option>
-              <mat-option *ngFor="let client of data.clients" [value]="client.id">
-                {{client.first_name}} {{client.last_name}}
-              </mat-option>
-            </mat-select>
-          </mat-form-field>
-        </div>
-        
-        <div class="form-group checkbox">
-          <mat-checkbox formControlName="is_scan">Is this a scan appointment?</mat-checkbox>
-        </div>
-      </form>
-    </mat-dialog-content>
-    <mat-dialog-actions align="end">
-      <button mat-button (click)="onCancel()">Cancel</button>
-      <button mat-raised-button color="primary" (click)="onSubmit()" [disabled]="eventForm.invalid">Save</button>
-    </mat-dialog-actions>
-  `,
-  styles: [`
-    .form-group {
-      margin-bottom: 15px;
-    }
-  `]
+  templateUrl: './create-event-form.component.html',
+  styleUrls: ['./create-event-form.component.scss']
 })
 
 
