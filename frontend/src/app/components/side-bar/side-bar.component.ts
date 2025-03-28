a03a3ce46d4d744e743f035bd7136931d39f4fa1
Daniel Villacis <Daniel1997v@gmail.com>
2025-03-28 03:17:00 -0400

    added the user's account in the bottom of the side bar for logout access

--- frontend/src/app/components/side-bar/side-bar.component.ts
+++ frontend/src/app/components/side-bar/side-bar.component.ts
@@ -1,13 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule, CommonModule, ThemeToggleComponent],
+  imports: [RouterModule, 
+    CommonModule, 
+    ThemeToggleComponent],
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss']
 })
-export class SideBarComponent {}
+export class SideBarComponent implements OnInit, OnDestroy {
+  user: any;
+  private userSubscription: Subscription | undefined;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userSubscription = this.authService.user$.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
