95d4024687cc41a7310769e463e9a47da197ac89
Daniel Villacis <Daniel1997v@gmail.com>
2025-04-06 19:52:17 -0400

    put the ai model in a dedicated folder for development

--- Backend/app/services/ai_service.py
+++ Backend/app/services/ai_service.py
@@ -1,10 +1,15 @@
 import subprocess
+import os
 
 class AIService:
     @staticmethod
     def run_model():
+
+        base_directory = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        model_path = os.path.join(base_directory, 'model', 'model.py')
+
         process = subprocess.Popen(
-            ['python3', 'model.py'], 
+            ['python3', model_path], 
             stdout=subprocess.PIPE, 
             stderr=subprocess.PIPE
         )
